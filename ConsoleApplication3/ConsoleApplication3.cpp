c31a3db8aaa986941b061a2c6a2ace47c40a1a23
Victor Manuel <victorakiladev@gmail.com>
2022-09-15 20:35:33 -0500

    avanze 7

--- ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -3,24 +3,51 @@
 using namespace std;
 
 int main() {
-    int x;
-    vector <int> response;
-    cout << "digite un numero: ";
-    cin >> x;
+    vector <vector<int>> response;
+	vector <int> remp;
+	vector <int> request;
+	for (size_t j = 1; j < 5; j++)
+	{
+		remp.push_back(j);
+	}
+	for (size_t i = 0; i < 4; i++)
+	{
+		response.push_back(remp);
+	}
+	for (size_t i = 0; i < response.size(); i++)
+	{
+		cout << "[";
+		for (size_t j = 0; j < response.size(); j++)
+		{
+			cout << response[i][j];
+		}
+		cout << "]";
+	}
+	for (size_t i = 0; i < response.size(); i++)
+	{
+		
+		for (size_t j = 0; j < response.size(); j++)
+		{
+			request.push_back(response[i][j]);
+		}
+		
+	}
+	int aux = 0;
+	for (size_t i = 0; i < request.size(); i++)
+	{
+		for (int j = 0; j < request.size() - 1; j++) {
+			if (request[j] > request[j + 1]) {
+				aux = request[j];
+				request[j] = request[j + 1];
+				request[j + 1] = aux;
+			}
+		}
+	}
+	
+	for (size_t i = 0; i < request.size(); i++)
+	{
+		cout << request[i];
+	}
 
-    for (int i = 1; i <= x+1; i++)
-    {
-        if(i % 2 == 0) {
-            response.insert(response.begin(), i);
-        }
-        if (i % 2 != 0) {
-            response.push_back(i);
-        }
-    }
-    for (int i = 0; i < response.size(); i++)
-    {
-        cout << response[i];
-    }
     return 0;
 }
-
