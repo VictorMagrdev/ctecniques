6dcf7c0e43abd27c569f2ea949df4cbad138999d
Victor Manuel <victorakiladev@gmail.com>
2022-09-15 19:04:41 -0500

    avanze 4

--- ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -3,7 +3,28 @@
 using namespace std;
 
 int main() {
-
+    vector <int> numb;
+    vector <int> response;
+    numb.push_back(6);
+    numb.push_back(4);
+    numb.push_back(2);
+    numb.push_back(1);
+    numb.push_back(3);
+    numb.push_back(5);
+    numb.push_back(7);
+    for (size_t i = 0; i < numb.size(); i++)
+    {
+        if(numb[i] % 2 == 0) {
+            response.insert(response.begin(), numb[i]);
+        }
+        if (numb[i] % 2 != 0) {
+            response.push_back(numb[i]);
+        }
+    }
+    for (size_t i = 0; i < response.size(); i++)
+    {
+        cout << response[i]<<endl;
+    }
     return 0;
 }
 
